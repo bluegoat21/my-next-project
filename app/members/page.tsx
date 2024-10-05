f3250f20b6e75d269c3221b69e65097f00092a02
bluegoat21 <info@bluegoat.jp>
2024-10-05 20:04:05 +0900

    4章まで終了

--- app/members/page.tsx
+++ app/members/page.tsx
@@ -1,7 +1,69 @@
+import styles from "./page.module.css";
+import Image from "next/image";
+
+const data = {
+  contents: [
+    {
+      id: "1",
+      image: {
+        url: "/img-member1.jpg",
+        width: 240,
+        height: 240,
+      },
+      name: "デイビッド・チャン",
+      position: "CEO",
+      profile: "ここにデイビッド・チャンのプロフィールが入ります。",
+    },
+    {
+      id: "2",
+      image: {
+        url: "/img-member2.jpg",
+        width: 240,
+        height: 240,
+      },
+      name: "エミリー・サンダース",
+      position: "COO",
+      profile: "ここにエミリー・サンダースのプロフィールが入ります。",
+    },
+    {
+      id: "2",
+      image: {
+        url: "/img-member3.jpg",
+        width: 240,
+        height: 240,
+      },
+      name: "ジョン・ウィルソン",
+      position: "CTO",
+      profile: "ここにジョン・ウィルソンのプロフィールが入ります。",
+    },
+  ],
+};
+
 export default function Page() {
   return (
-    <div style={{ margin: 20 }}>
-      <h1>メンバーページ</h1>
+    <div className={styles.container}>
+      {data.contents.length === 0 ? (
+        <p className={styles.empty}>メンバーが登録されていません。</p>
+      ) : (
+        <ul>
+          {data.contents.map((member) => (
+            <li key={member.id} className={styles.list}>
+              <Image
+                src={member.image.url}
+                alt=""
+                width={member.image.width}
+                height={member.image.height}
+                className={styles.image}
+              />
+              <dl>
+                <dt className={styles.name}>{member.name}</dt>
+                <dt className={styles.position}>{member.position}</dt>
+                <dt className={styles.profile}>{member.profile}</dt>
+              </dl>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
